5596578dcaa99549e55a44011c983e0edfe743e6
PabloRomeroMontero <pabloromeromontero23@gmail.com>
2020-01-14 11:49:22 +0100

    Commit de aula tic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ANIMALES} from '../data.animales';
+import {Animal} from '../interface/animal';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    animales: Animal[] = [];
 
-  constructor() {}
+    constructor() {
+        // Splice  crea una copìa del array animales
+        this.animales = ANIMALES.splice(0);
+    }
 
+    reproducir(animal: Animal) {
+        const audio = new Audio(); // Funcionalidad HTML
+        audio.src = animal.audio;
+
+        audio.load();
+        audio.play();
+
+        animal.reproduciendo = true;
+
+        setTimeout(() => animal.reproduciendo = false,
+            animal.duracion * 1000);
+    }
 }
